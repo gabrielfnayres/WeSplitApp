f2c7c4dfe4a81b4554a4803b23361a07e12e1097
Gabriel Ayres <gabn2012@gmail.com>
2024-12-24 13:55:54 -0300

    Initial Commit

---
+++ WeSplitApp/WeSplitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitAppApp.swift
+//  WeSplitApp
+//
+//  Created by Gabriel Ayres on 24/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
