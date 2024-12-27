6ad0406c9ef9507d8b3f32dd2d35d5b03d567fcf
Gabriel Ayres <gabn2012@gmail.com>
2024-12-27 12:00:24 -0300

    ïnitial commit

--- WeSplitApp/ContentView.swift
+++ WeSplitApp/ContentView.swift
@@ -8,14 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 30]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form{
+            Section{
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "BRL"))
+                    .keyboardType(.decimalPad)
+            }
+            
+            Section{
+                Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "BRL"))
+            }
         }
-        .padding()
     }
 }
 
